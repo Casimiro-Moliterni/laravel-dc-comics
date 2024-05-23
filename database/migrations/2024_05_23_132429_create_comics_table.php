8749fa8b1cfb4e94f8e5536a56a8f261793cc756
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-05-23 15:37:54 +0200

    add file arrow function comcis and migrate comics

---
+++ database/migrations/2024_05_23_132429_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');// title ->string
+            $table->text('description'); //description -> text
+            $table->text('image');//thumb -> string
+            $table->decimal('price',6,2);//price -> decimal
+            $table->string('string');//series -> string
+            $table->date('date');//sale_date -> date
+            $table->string('type');//type ->string
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
