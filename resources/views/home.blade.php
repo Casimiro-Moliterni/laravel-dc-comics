36c81636e65150fd1ca662081cf99c9868f2de6b
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-05-23 18:39:27 +0200

    add home page layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Home Page</h1>
+<body class="my_body">
+    <main>
+        <section>
+            <div class="my_hero">
+                 <div class="text-center">
+                    <h1>Home Page Dc Comics</h1>
+                 </div>
+                 <div class="container d-flex align-items-center h-100">
+                    <div class="d-flex w-100 gap-4 justify-content-center">
+                        <h3><a href="{{ route('comics.index') }}">Acquista i nostri fumetti</a></h3>
+                        <h3><a href="{{ route('comics.create') }}">Crea il tuo fumetto Personalizzato</a></h3>
+                    </div>
+                 </div>
+            </div>
+        </section>
+    </main>
+</body>
+
 @endsection
